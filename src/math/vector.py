320ae4db2ecab544e1c9b482955f4dfbcb16eb01
azamabdul <abdulkadirazam@gmail.com>
2025-07-19 22:49:04 -0700

    add vector projection

--- src/math/vector.py
+++ src/math/vector.py
@@ -24,12 +24,26 @@ class Vector():
             sum += v1.get(i) * v2.get(i)
         return sum
 
+    def apply_scalar(v, scalar):
+        result = Vector(v.size())
+        for i in range(v.size()):
+            result.set(i, v.get(i) * scalar)
+        return result
+
+    def project(onto, orig):
+        onto_magnitude = onto.l2_norm()
+        dot_prod = Vector.dot_product(onto, orig)
+
+        scale = dot_prod / onto_magnitude ** 2
+        return Vector.apply_scalar(onto, scale)
+
+
     def l2_norm(self):
         sum = 0
         for i in range(self.size()):
             sum += self.get(i) ** 2
         return math.sqrt(sum)
-
+    
     def print(self):
         print(self.vector)
 
@@ -40,3 +54,12 @@ v.print()
 
 print(Vector.dot_product(v, v))
 print(v.l2_norm())
+
+v2 = Vector(5)
+v2.set(0, 3)
+v2.set(1, 4)
+v2.set(2, 3)
+v2.print()
+print(v2.l2_norm())
+print(Vector.dot_product(v, v2))
+Vector.project(v2, v).print()
